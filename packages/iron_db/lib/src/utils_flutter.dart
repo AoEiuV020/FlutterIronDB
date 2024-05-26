5c52cf66024e44c8d1a3e67a3b540a92a162058d
AoEiuV020 <aoeiuv020@gmail.com>
2024-05-26 22:46:46 +0800

    读写assets支持windows

--- packages/iron_db/lib/src/utils_flutter.dart
+++ packages/iron_db/lib/src/utils_flutter.dart
@@ -33,14 +33,23 @@ Future<Database> getDebugAssetsDatabase(
     logger.warning('getDebugAssetsDatabase: not in debug mode');
     return DatabaseAssets(assetsBase, '', dataSerializer);
   }
+  if (Platform.isAndroid || Platform.isIOS) {
+    logger.warning('getDebugAssetsDatabase: not supported on Android or iOS');
+    return DatabaseAssets(assetsBase, '', dataSerializer);
+  }
+  final executable = Platform.resolvedExecutable;
+  logger.fine('executable: $executable');
   String? appProjectPath;
   if (Platform.isMacOS) {
     // .../FlutterIronDB/apps/example/build/macos/Build/Products/Debug/example.app/Contents/MacOS/example
-    final executable = Platform.resolvedExecutable;
     appProjectPath =
         path.canonicalize(path.join(executable, '../../../../../../../../../'));
     // mac必须手动选择目录后才有权限读写，所以这里会弹出选择框，选择项目所在目录，正常来说直接确定就好，
     appProjectPath = await getDirectoryPath(initialDirectory: appProjectPath);
+  } else if (Platform.isWindows || Platform.isLinux) {
+    // ...\FlutterIronDB\apps\example\build\windows\x64\runner\Debug\example.exe
+    appProjectPath =
+        path.canonicalize(path.join(executable, '../../../../../../'));
   }
   if (appProjectPath != null) {
     final assetsPath = path.join(appProjectPath, assetsBase);
